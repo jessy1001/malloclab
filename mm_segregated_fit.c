39fd2d1a968d31bbdeeaabfc53136ccea455ab60
Ubuntu <ubuntu@ip-172-31-46-172.ap-northeast-2.compute.internal>
2022-05-11 14:11:29 +0000

    segregated fit 정돈

--- mm_segregated_fit.c
+++ mm_segregated_fit.c
@@ -72,8 +72,8 @@ static void* seg_find(int size);
 
 
 void seg_init(void){
-	if ((seg_listp = mem_sbrk(32*WSIZE)) == (void*)-1) return -1;
-
+	if ((seg_listp = mem_sbrk(32*WSIZE)) == (void*)-1) return -1; //seg_list는 뒤로 늘릴 일이 없다는 전제 하에 만들기 때문에 heap_list보다 먼저 initiate.
+                                                                  //maximum free block size = 2^32일 거라는 가정 하에 그런 건데, ec2의 storage volume이 8GB인 데서 내린 결론.
 	for (int i = 0; i < 32; i++){
 		PUT(seg_listp + (i * WSIZE), NULL);
 	}
@@ -84,7 +84,7 @@ static void* seg_find(int size){
 
 	int i = 0;
 	for (i = 32; i > 0; i--){
-		if((size & (1<<i)) > 0){
+		if((size & (1<<i)) > 0){                //if size == 2^20 + blahblah, seg = 20
 			break;
 		}
 	}
@@ -97,18 +97,18 @@ int mm_init(void)
 {
 	seg_init();
 
-    if ((heap_listp = mem_sbrk(6 * WSIZE))== (void *)-1) return -1;
+    if ((heap_listp = mem_sbrk(4 * WSIZE))== (void *)-1) return -1;
     PUT(heap_listp, 0);                              //padding
-    PUT(heap_listp + WSIZE, PACK(2 * DSIZE, 1));     // header of prologue
-    PUT(heap_listp + 2 * WSIZE, NULL);               // point at prev free block
-    PUT(heap_listp + 3 * WSIZE, NULL);               // point at next free block
-    PUT(heap_listp + 4 * WSIZE, PACK(2 * DSIZE, 1)); // footer of prologue
-    PUT(heap_listp + 5 * WSIZE, PACK(0, 1));         // header of epilogue
+    PUT(heap_listp + WSIZE, PACK(DSIZE, 1));     // header of prologue
+    //PUT(heap_listp + 2 * WSIZE, NULL);               // point at prev free block
+    //PUT(heap_listp + 3 * WSIZE, NULL);               // point at next free block
+    PUT(heap_listp + 2 * WSIZE, PACK(DSIZE, 1)); // footer of prologue
+    PUT(heap_listp + 3 * WSIZE, PACK(0, 1));         // header of epilogue
 
     heap_listp += DSIZE;
     //free_listp = heap_listp; // 지울 것
 
-    if (extend_heap(CHUNKSIZE / DSIZE) == NULL)
+    if (extend_heap(CHUNKSIZE / DSIZE) == NULL)             //extending the heap by CHUNKSIZE is a convention derived from numerous practices...
     {
         return -1;
     }
@@ -170,16 +170,16 @@ static void* find_fit(size_t asize)
 	int i = 0;
 	for (i = 32; i > 0; i--){
 		if ((asize & (1<<i)) > 0){			//seg_find를 써도 될 것처럼 생겼으나
-			break;							//seg_find returns seg_listp + (i * WSIZE) and we need i.
+			break;							//seg_find returns seg_listp + (i * WSIZE) and we need i itself.
 		}
 	}
 
 	int j = i;
-	for(j = i; j <= 32; j++){
+	for(j = i; j <= 32; j++){               //j=i 부터 시작하는 이유는 size = 2^20 이렇게 딱 떨어지는 수일 수도 있으니까.(although 2^20 + blahblah is more likely)
 		seg = seg_listp + (j*WSIZE);
-		if(GET(seg) != NULL){
-			//best = (1<<(j + 1));
-			for(bp = PRED_FREEP(seg); bp != NULL; bp = PRED_FREEP(bp)){
+		if(GET(seg) != NULL){               //처음엔 extend_heap(CHUNKSIZE)이 배정된 seg=12까지 찾아감
+	
+			for(bp = PRED_FREEP(seg); bp != NULL; bp = PRED_FREEP(bp)){     //해당 seg 안에서도 바로 asize<=(size of block)인 블록이 안 나올 수도 있음
 				if (asize <= GET_SIZE(HDRP(bp))){   
 					return bp;             
 				}	                               
@@ -187,7 +187,7 @@ static void* find_fit(size_t asize)
 			}
         }
     }
-	return NULL;	
+	return NULL;	                        //find_fit 못 하면 extend_heap 할 거니까 return NULL 꼭 넣어줘야 됨, 이거 없으면 core gets dumped
 			
 }	
 
@@ -195,8 +195,7 @@ static void* find_fit(size_t asize)
 static void place(void *bp, size_t asize)
 {
     size_t csize = GET_SIZE(HDRP(bp));                     //current size
-    // 할당될 블록이니 가용리스트 내부에서 제거해준다.
-    removeBlock(bp);                                //remove block from free list
+    removeBlock(bp);                                       //remove block from free list
     if (csize - asize >= MIN_BLK_SIZE)
     {
         PUT(HDRP(bp), PACK(asize, 1));
@@ -204,10 +203,10 @@ static void place(void *bp, size_t asize)
         bp = NEXT_BLKP(bp);
         PUT(HDRP(bp), PACK(csize - asize, 0));
         PUT(FTRP(bp), PACK(csize - asize, 0));
-        // 가용리스트 첫번째에 분할된 블럭을 넣는다.
+        //pass leftover block into free list
         putFreeBlock(bp);
     }
-    else
+    else //if leftover block does not meet MIN_BLK_SIZE
     {
         PUT(HDRP(bp), PACK(csize, 1));
         PUT(FTRP(bp), PACK(csize, 1));
@@ -226,26 +225,26 @@ void mm_free(void *bp)
 void removeBlock(void *bp)
 {
 	if(PRED_FREEP(bp) != NULL){
-		PUT(PRED_FREEP(bp)+WSIZE, SUCC_FREEP(bp));
+		PUT(PRED_FREEP(bp)+WSIZE, SUCC_FREEP(bp));          //free list에서 내 pred의 succ을 내 succ로.
 	}
 	if(SUCC_FREEP(bp) != NULL){
-		PUT(SUCC_FREEP(bp), PRED_FREEP(bp));
+		PUT(SUCC_FREEP(bp), PRED_FREEP(bp));                //free list에서 내 succ의 pred를 내 pred로.
 	}
 }
 
-
+/* segregated fit 시 주목할 함수*/
 void putFreeBlock(void* bp)
 {
 	char* seg;
 	seg = seg_find(GET_SIZE(HDRP(bp)));
 
 	if(PRED_FREEP(seg) != NULL){
-		PUT(PRED_FREEP(seg)+WSIZE, bp);
+		PUT(PRED_FREEP(seg)+WSIZE, bp);                     //seg의 PRED 바로 뒤에 bp 넣기
 	}
-	PUT(bp, PRED_FREEP(seg));
-	PUT(bp + WSIZE, seg);
-	PUT(seg, bp);
-}
+	PUT(bp, PRED_FREEP(seg));                               //내 PRED는 seg의 PRED (그게 null이라면 null)
+	PUT(bp + WSIZE, seg);                                   //내 SUCC 는 seg
+	PUT(seg, bp);                                           //seg의 PRED는 나(bp)
+}                                                           
 
 static void *coalesce(void *bp)
 {
@@ -253,12 +252,12 @@ static void *coalesce(void *bp)
     size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
     size_t size = GET_SIZE(HDRP(bp));
 
-    // CASE 1
+    // CASE 1 : neither are available
     if (prev_alloc && next_alloc) {
         putFreeBlock(bp);
         return bp;
     }
-    // CASE 2 :
+    // CASE 2 : next block is available
     if (prev_alloc && !next_alloc)
     {
         removeBlock(NEXT_BLKP(bp));
@@ -267,7 +266,7 @@ static void *coalesce(void *bp)
         PUT(FTRP(bp), PACK(size, 0));
     }
 
-    // CASE 3 :
+    // CASE 3 : previous block is available
     else if (!prev_alloc && next_alloc)
     {
         removeBlock(PREV_BLKP(bp));
@@ -277,7 +276,7 @@ static void *coalesce(void *bp)
         PUT(FTRP(bp), PACK(size, 0));
     }
 
-    // CASE 4 :
+    // CASE 4 : both are available(unallocated)
     else //if (!prev_alloc && !next_alloc)
     {
         removeBlock(PREV_BLKP(bp));
@@ -288,7 +287,7 @@ static void *coalesce(void *bp)
         PUT(FTRP(bp), PACK(size, 0));
 
     }
-    //연결된 블록을 가용리스트에 추가
+    //add newly coalesced block to free list
     putFreeBlock(bp);
     return bp;
 }
@@ -303,7 +302,7 @@ void *mm_realloc(void *bp, size_t size)
         return mm_malloc(size);
     }
 
-    void *newp = mm_malloc(size);
+    void *newp = mm_malloc(size);           //*newp = malloc(size), copy data from old bp and free that bp, return newp
     if (newp == NULL){
         return 0;
     }
